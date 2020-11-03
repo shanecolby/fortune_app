dad23932df47b206ac2f0f4730fd919a41315e56
shane <shanecolbyzero8@gmail.com>
2020-11-02 20:31:35 -0600

    shows random fortunes

--- app/controllers/api/my_examples_controller.rb
+++ app/controllers/api/my_examples_controller.rb
@@ -1,5 +1,10 @@
 class Api::MyExamplesController < ApplicationController
   def fortune_action
+
+    fortunes = ["you will be wise", "You will be sleepy", "you will have lots of erroers"]
+    fortunes.sample
+    @fortune = fortunes.sample
+
     @time = Time.now.strftime('%b %e, %l:%M %p')
     @message = "This is a test"
     render "fortunetest.json.jb"
